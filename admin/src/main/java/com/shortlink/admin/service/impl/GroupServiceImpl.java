7bbe924d3bac9bd804e3e8c04b6a781b042beeb2
Chenfan Wu <2865051W@student.gla.ac.uk>
2024-04-01 21:07:51 +0100

    feature: 初始化短链接功能代码

---
+++ admin/src/main/java/com/shortlink/admin/service/impl/GroupServiceImpl.java
@@ -0,0 +1,19 @@
+package com.shortlink.admin.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.shortlink.admin.dao.entity.GroupDO;
+import com.shortlink.admin.dao.mapper.GroupMapper;
+import com.shortlink.admin.service.GroupService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * 短链接分组接口实现层
+ */
+@Service
+@Slf4j
+public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO>implements GroupService {
+
+
+
+}
