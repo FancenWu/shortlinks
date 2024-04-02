b02b0f50ac226f342cdddce89f889ef6649f8e2d
Chenfan Wu <2865051W@student.gla.ac.uk>
2024-04-02 19:07:59 +0100

    feature: 开发短链接后管系统分组集合

--- admin/src/main/java/com/shortlink/admin/service/impl/GroupServiceImpl.java
+++ admin/src/main/java/com/shortlink/admin/service/impl/GroupServiceImpl.java
@@ -1,14 +1,18 @@
 package com.shortlink.admin.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.shortlink.admin.dao.entity.GroupDO;
 import com.shortlink.admin.dao.mapper.GroupMapper;
+import com.shortlink.admin.dto.resp.ShortLinkGroupRespDTO;
 import com.shortlink.admin.service.GroupService;
 import com.shortlink.admin.toolkit.RandomGenerator;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * 短链接分组接口实现层
@@ -30,6 +34,18 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO>implement
                 .name(groupName)
                 .build();
         baseMapper.insert(groupDO);
+    }
+
+    @Override
+    public List<ShortLinkGroupRespDTO> listGroup() {
+        // TODO 从当前上下文获取用户名
+        LambdaQueryWrapper<GroupDO> queryWrapper = Wrappers.lambdaQuery(GroupDO.class)
+                .eq(GroupDO::getDelFlag, 0)
+                .eq(GroupDO::getUsername, "fancen")
+                .orderByDesc(GroupDO::getSortOrder, GroupDO::getUpdateTime);
+        List<GroupDO> groupDOList = baseMapper.selectList(queryWrapper);
+
+        return BeanUtil.copyToList(groupDOList, ShortLinkGroupRespDTO.class);
     }
 
     private boolean hasGid(String gid) {
