08526b29ea7ee80267e79a030e7ac808753daebb
Chenfan Wu <2865051W@student.gla.ac.uk>
2024-04-07 22:02:36 +0100

    feature: 短链接后管系统调用中台请求相关接口

---
+++ admin/src/main/java/com/shortlink/admin/remote/dto/resp/ShortLinkCreateRespDTO.java
@@ -0,0 +1,26 @@
+package com.shortlink.admin.remote.dto.resp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 短链接创建响应对象
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ShortLinkCreateRespDTO {
+
+    private String gid;
+
+
+    private String originUrl;
+
+
+    private String fullShortUrl;
+
+
+}
