ec709535c5f22fd24e1eb801cebe37ff72a432ee
Chenfan Wu <2865051W@student.gla.ac.uk>
2024-04-12 21:18:32 +0100

    feature: 短链接开发修改功能

---
+++ project/src/main/java/com/shortlink/project/dto/req/ShortLinkUpdateReqDTO.java
@@ -0,0 +1,34 @@
+package com.shortlink.project.dto.req;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * 短链接创建请求对象
+ */
+@Data
+public class ShortLinkUpdateReqDTO {
+
+
+    /**
+     * 原始链接
+     */
+    private String originUrl;
+
+    /**
+     * 完整短链接
+     */
+    private String fullShortUrl;
+
+    private String gid;
+
+
+    private Integer validDateType;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date validDate;
+
+    private String describe;
+}
