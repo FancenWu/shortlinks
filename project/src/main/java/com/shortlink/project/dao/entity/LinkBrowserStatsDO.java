2a4e0d862a1ff357f3e74dfc31502e571d7d39b1
Chenfan Wu <2865051W@student.gla.ac.uk>
2024-10-05 10:24:55 +0800

    feature: 开发短链接监控之浏览器访问数据

---
+++ project/src/main/java/com/shortlink/project/dao/entity/LinkBrowserStatsDO.java
@@ -0,0 +1,71 @@
+package com.shortlink.project.dao.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.shortlink.project.common.database.BaseDO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * 浏览器统计访问实体
+ */
+@Data
+@TableName("t_link_browser_stats")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LinkBrowserStatsDO extends BaseDO {
+
+    /**
+     * ID
+     */
+    private Long id;
+
+    /**
+     * 分组标识
+     */
+    private String gid;
+
+    /**
+     * 完整短链接
+     */
+    private String fullShortUrl;
+
+    /**
+     * 日期
+     */
+    private Date date;
+
+    /**
+     * 访问量
+     */
+    private Integer cnt;
+
+    /**
+     * 浏览器名称
+     */
+    private String browser;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 修改时间
+     */
+    private Date updateTime;
+
+    /**
+     * 删除标识 0:未删除 1:已删除
+     */
+    private Integer delFlag;
+
+
+
+
+
+}
