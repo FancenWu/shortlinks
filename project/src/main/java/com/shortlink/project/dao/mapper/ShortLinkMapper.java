242f47b9d8ca22f17836089e5e080ad72d3e93db
Chenfan Wu <2865051W@student.gla.ac.uk>
2024-10-07 16:14:07 +0800

    feature: 开发短链接监控之分页排序功能

--- project/src/main/java/com/shortlink/project/dao/mapper/ShortLinkMapper.java
+++ project/src/main/java/com/shortlink/project/dao/mapper/ShortLinkMapper.java
@@ -1,7 +1,9 @@
 package com.shortlink.project.dao.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.shortlink.project.dao.entity.ShortLinkDO;
+import com.shortlink.project.dto.req.ShortLinkPageReqDTO;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Update;
 
@@ -20,5 +22,10 @@ public interface ShortLinkMapper extends BaseMapper<ShortLinkDO> {
             @Param("totalUv") Integer totalUv,
             @Param("totalUip") Integer totalUip
     );
+
+    /**
+     * 分页统计短链接
+     */
+    IPage<ShortLinkDO> pageLink(ShortLinkPageReqDTO requestParam);
 
 }
