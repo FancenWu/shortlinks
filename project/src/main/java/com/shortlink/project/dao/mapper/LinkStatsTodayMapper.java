885e9f8f299d2aaf405986c37dcd867f25c6b32a
Chenfan Wu <2865051W@student.gla.ac.uk>
2024-10-07 12:21:42 +0800

    feature: 开发短链接分页监控相关功能

---
+++ project/src/main/java/com/shortlink/project/dao/mapper/LinkStatsTodayMapper.java
@@ -0,0 +1,9 @@
+package com.shortlink.project.dao.mapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.shortlink.project.dao.entity.LinkStatsTodayDO;
+
+/**
+ * 短链接今日统计持久层
+ */
+public interface LinkStatsTodayMapper extends BaseMapper<LinkStatsTodayDO> {
+}
