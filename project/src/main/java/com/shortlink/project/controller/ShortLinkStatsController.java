1637284a1619f93bc46167d5368086c64313573a
Chenfan Wu <2865051W@student.gla.ac.uk>
2024-10-07 08:47:52 +0800

    feature: 开发短链接监控之分页访问记录接口

--- project/src/main/java/com/shortlink/project/controller/ShortLinkStatsController.java
+++ project/src/main/java/com/shortlink/project/controller/ShortLinkStatsController.java
@@ -1,8 +1,11 @@
 package com.shortlink.project.controller;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.shortlink.project.common.convention.result.Result;
 import com.shortlink.project.common.convention.result.Results;
+import com.shortlink.project.dto.req.ShortLinkStatsAccessRecordReqDTO;
 import com.shortlink.project.dto.req.ShortLinkStatsReqDTO;
+import com.shortlink.project.dto.resp.ShortLinkStatsAccessRecordRespDTO;
 import com.shortlink.project.dto.resp.ShortLinkStatsRespDTO;
 import com.shortlink.project.service.ShortLinkStatsService;
 import lombok.RequiredArgsConstructor;
@@ -25,4 +28,14 @@ public class ShortLinkStatsController {
     public Result<ShortLinkStatsRespDTO> shortLinkStats(ShortLinkStatsReqDTO requestParam) {
         return Results.success(shortLinkStatsService.oneShortLinkStats(requestParam));
     }
+
+    /**
+     * 访问单个短链接指定时间内访问记录监控数据
+     */
+    @GetMapping("/api/short-link/v1/stats/access-record")
+    public Result<IPage<ShortLinkStatsAccessRecordRespDTO>> shortLinkStatsAccessRecord(ShortLinkStatsAccessRecordReqDTO requestParam) {
+        return Results.success(shortLinkStatsService.shortLinkStatsAccessRecord(requestParam));
+    }
+
+
 }
